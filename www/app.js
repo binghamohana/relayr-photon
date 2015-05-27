d529372630121d520aad124d1671b416a22e47fc
Khaled Osman <khaled.osmaan@gmail.com>
2015-05-27 13:29:57 +0200

    rebuild app with plain javascript for simplicity

--- www/app.js
+++ www/app.js
@@ -1,16 +1,21 @@
-var myApp = angular.module('gasDetector', []);
-
-myApp.controller('gasLevelController', ['$scope', function($scope){
-	var relayr = RELAYR.init({
+var p = document.getElementById('test');
+var devId = "5439a96f-f58a-4ec2-ad3b-6f74526acaee";
+var relayr = RELAYR.init({
     appId: "45e676b0-a93e-469f-b01d-6e985466369a"
- 	});
- 	  relayr.devices().getDeviceData({
-    deviceId: "c36642b8-3327-4935-8f21-19a0196e7349", 
+  });
+    relayr.devices().getDeviceData({
+    deviceId: devId, 
     token: "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes",
     incomingData: function(data){
       console.log("sensor",data);
-      $scope.gasLevel=data.readings.value;
+      p.innerText="My moisture leve is " + data.readings[0].value;
     }
-  });    
-	//$scope.gasLevel='Dummy gas sensor value';
-}]);
+  });  
+
+  var changeColor=function(color){
+    relayr.devices().sendCommand({
+    deviceId: devId, 
+    //token: "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes",
+    body:'{"Color":"RED"}'
+    })
+  };
