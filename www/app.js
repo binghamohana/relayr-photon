372e08bae935f9477717c54bd977e23e053ef07d
Khaled Osman <khaled.osmaan@gmail.com>
2015-05-27 14:11:28 +0200

    implemented send command

--- www/app.js
+++ www/app.js
@@ -1,11 +1,12 @@
 var p = document.getElementById('test');
 var devId = "5439a96f-f58a-4ec2-ad3b-6f74526acaee";
+var tok =  "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes";
 var relayr = RELAYR.init({
     appId: "45e676b0-a93e-469f-b01d-6e985466369a"
   });
     relayr.devices().getDeviceData({
     deviceId: devId, 
-    token: "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes",
+    token: tok,
     incomingData: function(data){
       console.log("sensor",data);
       p.innerText="My moisture leve is " + data.readings[0].value;
@@ -13,9 +14,10 @@ var relayr = RELAYR.init({
   });  
 
   var changeColor=function(color){
-    relayr.devices().sendCommand({
-    deviceId: devId, 
-    //token: "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes",
-    body:'{"Color":"RED"}'
-    })
-  };
+    var device = {
+      deviceId: devId,
+      token: tok,
+      command: {"Color":color}
+    }
+    relayr.devices().sendCommand(device);
+    }
