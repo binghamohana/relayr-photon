94df694473e8cf040544dab7079b8798d3580be3
Khaled Osman <khaled.osmaan@gmail.com>
2015-05-27 14:32:31 +0200

    update code documentation

--- photon-relayr.ino
+++ photon-relayr.ino
@@ -7,9 +7,9 @@
 #define MQTT_SERVER "mqtt.relayr.io"
 #define MQTT_CLIENTID "photon-relayr" //can be anything else
 
-char message_buff[100];
 const int led = D7;
 const int sensorPin = A0;
+char message_buff[100];
 unsigned long lastTime = 0;
 
                                 // Set here the time in milliseconds between publications
@@ -19,21 +19,21 @@ int publishingPeriod = 1000;    // ATTENTION !!!
 
 void callback(char* topic, byte* payload, unsigned int length);
 
-//create our mqtt client
+//create our mqtt client object
 MQTT client(MQTT_SERVER,1883,callback);
 
-// define our callback method thats called on receiving data
+// implement our callback method thats called on receiving data
 void callback(char* topic, byte* payload, unsigned int length) {
-
     //blink for 1 second on receiving data
     blink(1000);
-    //store the received payload
+    //store the received payload and print it at the Serial port
     char p[length + 1];
     memcpy(p, payload, length);
     p[length] = NULL;
     String message(p);
     Serial.println(message);
 
+//handle message received
 //change the photon's RGB LED according to payload received
     if (message.equals("{\"Color\":\"RED\"}"))
         RGB.color(255, 0, 0);
@@ -42,21 +42,20 @@ void callback(char* topic, byte* payload, unsigned int length) {
     else if (message.equals("{\"Color\":\"BLUE\"}"))
         RGB.color(0, 0, 255);
     else
-    //if unexpected payload LED color to white
+    //if different payload change LED color to white
         RGB.color(255, 255, 255);
     delay(100);
 }
 
-void mqtt_connect(){
+void mqtt_connect() {
   Serial.println("Connecting to mqtt server");
   if(client.connect(MQTT_CLIENTID, MQTT_USER, MQTT_PASSWORD)) {
-    Serial.println("Connection success");
-
+    Serial.println("Connection success, subscribing to topic");
     //subscribe to a topic, cmd in this case
     client.subscribe("/v1/"DEVICE_ID"/cmd");
   }
   else {
-    Serial.println("Connection failed, check your credentials or wifi");
+    Serial.println("Connection failed, check your credentials");
   }
 }
 
@@ -66,6 +65,7 @@ void setup() {
     Serial.println("Hello There, I'm your photon!");
     //setup our LED pin and connect to mqtt broker
     pinMode(led, OUTPUT);
+    //set 200ms as minimum publishing period
     publishingPeriod = publishingPeriod > 200 ? publishingPeriod : 200;
     mqtt_connect();
 }
@@ -73,6 +73,7 @@ void setup() {
 void loop() {
     if (client.isConnected()) {
         client.loop();
+        //publish within publishing period
         if (millis() - lastTime > publishingPeriod) {
         lastTime = millis();
         publish();
@@ -84,11 +85,13 @@ void loop() {
 }
 
 void publish() {
-    //publish to the out topic
+    //create our json payload
     String pubString = "{\"meaning\":\"moisture\", \"value\":";
+    //read and add sensor data to payload
     pubString += analogRead(sensorPin);
     pubString += "}";
     pubString.toCharArray(message_buff, pubString.length()+1);
+    //publish our json payload
     client.publish("/v1/"DEVICE_ID"/data", message_buff);
     Serial.println("Publishing");
     //100ms blink when publishing
