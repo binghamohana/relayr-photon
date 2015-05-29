9d6504f6e1dd5993595bf06042e2694e479bf3c6
Paul Hopton <paul@relayr.de>
2015-05-29 10:55:45 +0200

    cleanup on app.js
    Tweaked varibale names for clarity
    forced uppercase on color value in command

--- www/app.js
+++ www/app.js
@@ -1,24 +1,24 @@
-var p = document.getElementById('reading');
+var reading = document.getElementById('reading');
 var devId = "5439a96f-f58a-4ec2-ad3b-6f74526acaee";
-var tok =  "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes";
+var token =  "lCS.8toUuj67toE.Uh4YX-xhSY7lSDes";
 var relayr = RELAYR.init({
     appId: "45e676b0-a93e-469f-b01d-6e985466369a"
 });
 
 relayr.devices().getDeviceData({
    deviceId: devId, 
-   token: tok,
+   token: token,
    incomingData: function(data){
      console.log("sensor",data);
-     p.innerText="My moisture reading is " + data.readings[0].value;
+     reading.innerText="My moisture reading is " + data.readings[0].value;
    }
 });  
 
 var changeColor = function(color) {
   var device = {
     deviceId: devId,
-    token: tok,
-    command: {"Color":color}
+    token: token,
+    command: {"color":color.toUpperCase()}
   };
   relayr.devices().sendCommand(device);
 };
