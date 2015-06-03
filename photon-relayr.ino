c7b59a8ac4592d01dd6ebb8b8f3037d84a031e03
Khaled Osman <khaled.osmaan@gmail.com>
2015-06-03 10:51:34 +0200

    code cleaning

--- photon-relayr.ino
+++ photon-relayr.ino
@@ -1,4 +1,3 @@
-// This #include statement was automatically added by the Spark IDE.
 #include "SparkJson/SparkJson.h"
 #include "MQTT.h"
 
@@ -16,60 +15,63 @@ char message_buff[100];
 unsigned long lastPublishTime = 0;
 unsigned long lastBlinkTime = 0;
 
-										// Set here the time in milliseconds between publications
-int publishingPeriod = 1000;  // ATTENTION !!!
-										// DO NOT try to set values under 200 ms of the server
-										// will kick you out
+														// Set here the time in milliseconds between publications
+int publishingPeriod = 1000;					// ATTENTION !!!
+														// DO NOT try to set values under 200 ms of the server
+														// will kick you out
 
 void callback(char* topic, byte* payload, unsigned int length);
 
-//create our mqtt client object
+//create our instance of MQTT object
 MQTT client(MQTT_SERVER,1883,callback);
 
-// implement our callback method thats called on receiving data
+//implement our callback method thats called on receiving data from a subscribed topic
 void callback(char* topic, byte* payload, unsigned int length) {
 	//store the received payload and convert it to string
 	char p[length + 1];
 	memcpy(p, payload, length);
 	p[length] = NULL;
-	String message(p);
 	//print the topic and the payload received
 	Serial.println("topic: " + String(topic));
-	Serial.println("payload: " + message);
-	//call our method to do something with the payload received
+	Serial.println("payload: " + String(p));
+	//call our method to parse and use the payload received
 	handlePayload(p);
 }
 
 void handlePayload(char* payload) {
-     StaticJsonBuffer<200> jsonBuffer;
-     JsonObject& root = jsonBuffer.parseObject(payload);
-  if (!root.success()) {
-    Serial.println("json parsing failed");
-    return;
-  }
-  const char* command = root["command"];
-  if(String(command).equals("color"))
-  {
-    const char* color = root["value"];  
-        String s(color);
-        if(s.equals("red")) 
-        RGB.color(255,0,0);
-        else if(s.equals("blue"))
-        RGB.color(0,0,255);
-        else if(s.equals("green"))
-        RGB.color(0,255,0);
-  }
+	StaticJsonBuffer<200> jsonBuffer;
+	//convert payload to json
+	JsonObject& json = jsonBuffer.parseObject(payload);
+	if (!json.success()) {
+		Serial.println("json parsing failed");
+		return;
+	}
+	//get value of the key "command"
+	const char* command = json["command"];
+	Serial.println("parsed command: " + String(command));
+		if(String(command).equals("color"))
+	  {
+		  const char* color = json["value"];  
+		  Serial.println("parsed color: " + String(color));
+		  String s(color);
+		  if(s.equals("red")) 
+				RGB.color(255,0,0);
+		  else if(s.equals("blue"))
+				RGB.color(0,0,255);
+		  else if(s.equals("green"))
+				RGB.color(0,255,0);
+	 }
 }
 
 void mqtt_connect() {
   Serial.println("Connecting to mqtt server");
   if(client.connect(MQTT_CLIENTID, MQTT_USER, MQTT_PASSWORD)) {
-	Serial.println("Connection success, subscribing to topic");
-	//subscribe to a topic
-	client.subscribe("/v1/"DEVICE_ID"/cmd");
-  }
+		Serial.println("Connection success, subscribing to topic");
+		//subscribe to a topic
+		client.subscribe("/v1/"DEVICE_ID"/cmd");
+	}
   else {
-	Serial.println("Connection failed, check your credentials");
+		Serial.println("Connection failed, check your credentials or wifi");
   }
 }
 
@@ -93,20 +95,25 @@ void loop() {
 			publish();
 		}
 		blink(publishingPeriod/2);
-	} else {
+	} 
+	else {
 		//if connection lost, reconnect
-		 mqtt_connect();
+		Serial.println("retrying..");
+		mqtt_connect();
 	}
 }
 
 void publish() {
-  StaticJsonBuffer<100> pubJsonBuffer;
-  JsonObject& pubJson = pubJsonBuffer.createObject();
-  pubJson["meaning"] = "moisture";
-  pubJson["value"] = analogRead(sensorPin);
-	
+	//create our json payload as {"meaning":"moisture","value":sensorvalue}
+	StaticJsonBuffer<100> pubJsonBuffer;
+	JsonObject& pubJson = pubJsonBuffer.createObject();
+	//define a meaning for what we're sending
+	pubJson["meaning"] = "moisture";
+	//set our value key to the sensor's reading
+	pubJson["value"] = analogRead(sensorPin);
+	//copy our json object as a char array and publish it via mqtt
 	char message_buff[100];
-   pubJson.printTo(message_buff, sizeof(message_buff));
+	pubJson.printTo(message_buff, sizeof(message_buff));
 	client.publish("/v1/"DEVICE_ID"/data", message_buff);
 	Serial.println("Publishing " + String(message_buff));
 }
@@ -123,3 +130,4 @@ void blink(int interval) {
 		digitalWrite(led, ledState);
 	}
 }
+
